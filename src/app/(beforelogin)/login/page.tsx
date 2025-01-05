496852cbb533c80ce4479f6289c9bf1226a5366d
soyeooooo <alththdus1@naver.com>
2025-01-05 21:34:36 +0900

    feat : modal
    회원가입 , 로그인 모달 설정

--- src/app/(beforelogin)/login/page.tsx
+++ src/app/(beforelogin)/login/page.tsx
@@ -1,5 +1,15 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from "next/navigation";
+import Main from "@/app/(beforelogin)/_component/main"
 
 export default function Login() {
-  redirect('/i/flow/login');
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.replace('/i/flow/login');
+  }, [router]);
+
+  return <Main />;
 }
